aacbd40839e8de2869f07a9acf2af3a720c9f40b
Joao Vitor Mineiro <joaovitor_reismineiro@hotmail.com>
2021-05-23 18:18:28 -0300

    Organizando room com controller

--- app/pages/room/src/index.js
+++ app/pages/room/src/index.js
@@ -1,17 +1,6 @@
 import { constants } from "../../_shared/constants.js"
+import RoomController from "./controller.js"
 import RoomSocketBuilder from "./util/roomSocket.js"
-
-
-const socketBuilder = new RoomSocketBuilder({
-    socketUrl: constants.socketUrl,
-    namespace: constants.socketNamespaces.room
-})
-
-const socket = socketBuilder
-    .setOnUserConnected((user) => console.log('user connected!', user))
-    .setOnUserDisconnected((user) => console.log('user disconnected!', user))
-    .setOnRoomUpdated((room) => console.log('room list!', room))
-    .build()
 
 const room = {
     id: '0001',
@@ -22,5 +11,21 @@ const user = {
     img: 'https://cdn4.iconfinder.com/data/icons/avatars-xmas-giveaway/128/bear_russian_animal_avatar-256.png',
     username: 'Erick ' + Date.now()
 }
+const roomInfo = {user, room}
+const socketBuilder = new RoomSocketBuilder({
+    socketUrl: constants.socketUrl,
+    namespace: constants.socketNamespaces.room
+})
+
+const roomController = new RoomController({
+    socketBuilder,
+})
+
+const socket = socketBuilder
+    .setOnUserConnected((user) => console.log('user connected!', user))
+    .setOnUserDisconnected((user) => console.log('user disconnected!', user))
+    .setOnRoomUpdated((room) => console.log('room list!', room))
+    .build()
+
 
 socket.emit(constants.events.JOIN_ROOM, { user, room })
