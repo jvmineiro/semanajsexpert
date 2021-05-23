296fdbb74f18d29f73ba16324011783a63a37181
Joao Vitor Mineiro <joaovitor_reismineiro@hotmail.com>
2021-05-23 18:35:32 -0300

    Atualizando as imagens do USER

--- app/pages/room/src/controller.js
+++ app/pages/room/src/controller.js
@@ -1,19 +1,24 @@
 import {constants} from "../../_shared/constants.js";
 
 export default class RoomController{
-    constructor({roomInfo, socketBuilder }) {
+    constructor({roomInfo, socketBuilder, view }) {
         this.socketBuilder = socketBuilder
         this.roomInfo = roomInfo
+        this.view = view
         this.socket = {}
     }
     static async initialize(deps){
         return new RoomController(deps)._initialize()
     }
     async _initialize() {
+        this._setupViewEvents()
         this.socket = this._setupSocket()
 
-
         this.socket.emit(constants.events.JOIN_ROOM, this.roomInfo)
+    }
+
+    _setupViewEvnts() {
+        this.view.updateUserImage(this.roomInfo.user)
     }
 
     _setupSocket() {
--- app/pages/room/src/index.js
+++ app/pages/room/src/index.js
@@ -1,6 +1,7 @@
 import { constants } from "../../_shared/constants.js"
 import RoomController from "./controller.js"
 import RoomSocketBuilder from "./util/roomSocket.js"
+import View from "./view.js"
 
 const room = {
     id: '0001',
@@ -18,9 +19,12 @@ const socketBuilder = new RoomSocketBuilder({
 })
 
 const dependencies = {
+    view: View,
     socketBuilder,
     roomInfo
 }
+
+
 
 await RoomController.initialize(dependencies)
 
